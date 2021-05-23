29c2e32fbb1772be62501f062c58bcad31c4de3a
Elie Benzaquen <eliebenzaquen@gmail.com>
2021-05-23 13:36:20 -0400

    final touches

--- Server.java
+++ Server.java
@@ -1,4 +1,3 @@
-import java.rmi.*;
 import java.rmi.registry.*;
 
 public class Server {
@@ -6,29 +5,9 @@ public class Server {
         
         Impl obj = new Impl();
         obj.initiate(args[0]);
-        // StatImpl statImpl = new StatImpl();
-        // SizeImpl sizeImpl = new SizeImpl();
-        // ReadImpl readImpl = new ReadImpl();
-        // OpenImpl openImpl = new OpenImpl();
-        // CloseImpl closeImpl = new CloseImpl();
-        // LSImpl lsImpl = new LSImpl();
-        // InfoImpl infoImpl = new InfoImpl();
-        // CDImpl cdImpl = new CDImpl();
-        // InitImpl initImpl = new InitImpl();
-        
-
 
         Registry registry = LocateRegistry.getRegistry("192.168.130.3");
-        // Registry registry = LocateRegistry.getRegistry(2150);
-        // registry.rebind("Stat", statImpl);
-        // registry.rebind("Size", sizeImpl);
-        // registry.rebind("Read", readImpl);
-        // registry.rebind("Open", openImpl);
-        // registry.rebind("LS", lsImpl);
-        // registry.rebind("Info", infoImpl);
-        // registry.rebind("Close", closeImpl);
-        // registry.rebind("CD", cdImpl);
-        // registry.rebind("Init", initImpl);
+
         registry.rebind("Fat32Reader", obj);
 
         System.out.println("Server Started");
